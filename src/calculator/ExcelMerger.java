1706b594d4acbdfbb22ad886fba29c5734b75500
JIMLU001234 <xm3ruxup6vupm4@gmail.com>
2024-05-01 00:07:22 +0900

    fix duplicated header bug

--- src/calculator/ExcelMerger.java
+++ src/calculator/ExcelMerger.java
@@ -5,23 +5,26 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.*;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ExcelMerger {
 
     public static void main(String[] args) {
-        mergeExcelSheets(Arrays.asList("D:\\test_files\\excel_combined\\work\\SDR1.xlsx", "D:\\test_files\\excel_combined\\work\\SDR2.xlsx"), "D:\\test_files\\excel_combined\\output\\merged.xlsx");
+    	mergeExcelSheets(Arrays.asList("D:\\test_files\\excel_combined\\work\\SDR1.xlsx", "D:\\test_files\\excel_combined\\work\\SDR2.xlsx","D:\\test_files\\excel_combined\\work\\SDR3.xlsx"), "D:\\test_files\\excel_combined\\output\\merged.xlsx");
     }
 
     public static void mergeExcelSheets(List<String> inputFiles, String outputFile) {
         try (FileOutputStream fos = new FileOutputStream(outputFile)) {
 
             Workbook mergedWorkbook = new XSSFWorkbook();
+            Set<String> mergedSheetNames = new HashSet<>();
 
             for (String inputFile : inputFiles) {
                 try (FileInputStream fis = new FileInputStream(inputFile)) {
                     Workbook workbook = new XSSFWorkbook(fis);
-                    mergeSheets(workbook, mergedWorkbook);
+                    mergeSheets(workbook, mergedWorkbook, mergedSheetNames);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -35,19 +38,40 @@ public class ExcelMerger {
         }
     }
 
-    private static void mergeSheets(Workbook sourceWorkbook, Workbook targetWorkbook) {
+    private static void mergeSheets(Workbook sourceWorkbook, Workbook targetWorkbook, Set<String> mergedSheetNames) {
         for (int i = 0; i < sourceWorkbook.getNumberOfSheets(); i++) {
             Sheet sourceSheet = sourceWorkbook.getSheetAt(i);
-            Sheet targetSheet = targetWorkbook.getSheet(sourceSheet.getSheetName());
-            if (targetSheet == null) {
-                targetSheet = targetWorkbook.createSheet(sourceSheet.getSheetName());
+            String sheetName = sourceSheet.getSheetName();
+
+            if (!mergedSheetNames.contains(sheetName)) {
+                Sheet targetSheet = targetWorkbook.createSheet(sheetName);
+                copySheet(sourceSheet, targetSheet);
+                mergedSheetNames.add(sheetName);
+            } else {
+                // Skip copying header for already merged sheets
+                copySheetWithoutHeader(sourceSheet, targetWorkbook.getSheet(sheetName));
             }
-            copySheet(sourceSheet, targetSheet);
         }
     }
 
     private static void copySheet(Sheet sourceSheet, Sheet targetSheet) {
         for (int i = 0; i <= sourceSheet.getLastRowNum(); i++) {
+            Row sourceRow = sourceSheet.getRow(i);
+            Row targetRow = targetSheet.createRow(targetSheet.getLastRowNum() + 1);
+            if (sourceRow != null) {
+                for (int j = 0; j <= sourceRow.getLastCellNum(); j++) {
+                    Cell sourceCell = sourceRow.getCell(j);
+                    if (sourceCell != null) {
+                        Cell targetCell = targetRow.createCell(j, sourceCell.getCellType());
+                        copyCellValue(sourceCell, targetCell);
+                    }
+                }
+            }
+        }
+    }
+
+    private static void copySheetWithoutHeader(Sheet sourceSheet, Sheet targetSheet) {
+        for (int i = 1; i <= sourceSheet.getLastRowNum(); i++) {
             Row sourceRow = sourceSheet.getRow(i);
             Row targetRow = targetSheet.createRow(targetSheet.getLastRowNum() + 1);
             if (sourceRow != null) {
